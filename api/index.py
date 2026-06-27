9b95c74cab565ca4438463a919e4e41ffad74aaf
usama <70141025@student.uol.edu.pk>
2026-06-27 06:38:34 +0500

    config for production

---
+++ api/index.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
+
+from app import app
+
+if __name__ == "__main__":
+    app.run(debug=False)
